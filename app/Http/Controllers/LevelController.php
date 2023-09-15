c70f44737e9c582f89aac00e6050684326fb2d32
ET-112 <monu.tyagi@echttech.com>
2023-09-15 11:35:21 +0530

    level controller

--- app/Http/Controllers/LevelController.php
+++ app/Http/Controllers/LevelController.php
@@ -437,7 +437,7 @@ class LevelController extends Controller
 
   public function level1tp(Request $request,$id=null)
   {
-    //dd($id);
+
         //dd("we are work on manage ");
         $form_step_type= Session::get('session_for_redirections');
 
