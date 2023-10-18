df8aadf8924d585a8e7e9c0e1fd1bfc51fdc0448
chahal1997 <surinder.singh@echttech.com>
2023-10-18 11:36:05 +0530

    remove CDN from header and footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -5,11 +5,11 @@
                 </div>
             </div>
         </section>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
 {{-- state dropdown --}}
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
+<script src="{{ asset('assets/js/atab-popper.min.js') }}" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="{{ asset('assets/js/atab-bootstrap.min.js') }}" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="{{ asset('assets/js/atab-jquery.validate.min.js') }}"></script>
 
 <script>
     $(document).ready(function() {
@@ -339,13 +339,13 @@ $("document").ready(function(){
 
 
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.0.3/fullcalendar.min.js"></script>
+<script src="{{ asset('assets/js/atab-fullcalendar.min.js') }}"></script>
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.js"></script>  -->
 
 <script src="{{ asset('assets/js/table.min.js') }}"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
- <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+<script src="{{ asset('assets/js/atab-moment.min.js') }}"></script>
+ <script src="{{ asset('assets/js/atab-toastr.min.js') }}"></script>
 <!-- <script src="{{ asset('assets/js/pages/tables/jquery-datatable.js') }}"></script> -->
 
 
@@ -353,12 +353,12 @@ $("document").ready(function(){
 
 
 
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js "></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
-    <script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+    <script src="{{ asset('assets/js/atab-dataTables.buttons.min.js') }} "></script>
+    <script src="{{ asset('assets/js/atab-jszip.min.js') }}"></script>
+    <script src="{{ asset('assets/js/atab-pdfmake.min.js') }}"></script>
+    <script src="{{ asset('assets/js/atab-vfs_fonts.js') }}"></script>
+    <script src="{{ asset('assets/js/atab-buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('assets/js/atab-buttons.print.min.js') }}"></script>
       <script >
          $(document).ready(function() {
        $('.js-basic-example').DataTable( {
