3e38f578abba5ec2c3dc30ad73dad7599126d331
suraj.echttech <suraj.chauhan@echttech.com>
2024-07-30 15:50:19 +0530

    pre-changes

--- app/Http/Controllers/application_controller/SuperAdminApplicationController.php
+++ app/Http/Controllers/application_controller/SuperAdminApplicationController.php
@@ -1526,6 +1526,7 @@ class SuperAdminApplicationController extends Controller
                       ->on('tbl_course_wise_document.doc_unique_id', '=', 'sub.doc_unique_id')
                       ->on('tbl_course_wise_document.id', '=', 'sub.max_id');
               })
+              ->where('tbl_course_wise_document.application_id',$application_id)
               ->orderBy('tbl_course_wise_document.id', 'desc')
               ->get(['tbl_course_wise_document.application_id', 'tbl_course_wise_document.course_id', 'tbl_course_wise_document.doc_sr_code', 'tbl_course_wise_document.doc_unique_id', 'tbl_course_wise_document.status', 'id', 'admin_nc_flag','approve_status','is_revert']);
   
