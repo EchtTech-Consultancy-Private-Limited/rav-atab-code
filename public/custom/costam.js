ff188b9a4217e24e3310d920d96811a4dd6f48ff
suraj.echttech <suraj.chauhan@echttech.com>
2024-08-16 11:31:54 +0530

    implemented offline/online btn

--- public/custom/costam.js
+++ public/custom/costam.js
@@ -6,5 +6,18 @@ $("document").ready(function(){
        $("div.alert").remove();
     }, 5000 ) // 5 secs
 
+    $(".tooltip-btn-pay").on('click',function(){
+        alert("hhhhh")
+        $(this).toggleClass('active')
+      })
+      
+    
 });
+
+function f1(){
+    alert("ffff")
+  }
 //password policy 
+
+
+
